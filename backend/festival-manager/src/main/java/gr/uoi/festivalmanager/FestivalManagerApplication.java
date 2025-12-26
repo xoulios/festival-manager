8b911cf0deaff6c720b6e1e0db52990463d6d991
Xoulios <seymenbuba2004@gmail.com>
2025-12-26 20:38:11 +0200

    exceptions & test

---
+++ backend/festival-manager/src/main/java/gr/uoi/festivalmanager/FestivalManagerApplication.java
@@ -0,0 +1,13 @@
+package gr.uoi.festivalmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalManagerApplication.class, args);
+	}
+
+}
---
+++ backend/festival-manager/src/test/java/gr/uoi/festivalmanager/FestivalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uoi.festivalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
