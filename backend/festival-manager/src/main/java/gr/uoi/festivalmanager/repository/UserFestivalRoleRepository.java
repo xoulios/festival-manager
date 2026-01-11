bb9c5ac37fa6d5377df14ae7bb48d2a5a6b34ae7
Xoulios <seymenbuba2004@gmail.com>
2026-01-11 23:40:29 +0200

    diorthosi auth issues

--- backend/festival-manager/src/main/java/gr/uoi/festivalmanager/repository/UserFestivalRoleRepository.java
+++ backend/festival-manager/src/main/java/gr/uoi/festivalmanager/repository/UserFestivalRoleRepository.java
@@ -4,7 +4,8 @@ import gr.uoi.festivalmanager.entity.UserFestivalRole;
 import gr.uoi.festivalmanager.entity.UserFestivalRoleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import gr.uoi.festivalmanager.dto.AuthMeResponse;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,4 +20,13 @@ public interface UserFestivalRoleRepository extends JpaRepository<UserFestivalRo
 
     @Query("select ufr from UserFestivalRole ufr join fetch ufr.role join fetch ufr.festival where ufr.user.id = :userId")
     List<UserFestivalRole> findAllByUserId(Long userId);
+    
+    @Query("""
+    select new gr.uoi.festivalmanager.dto.AuthMeResponse$FestivalRole(ufr.id.festivalId, ufr.role.name)
+    from UserFestivalRole ufr
+    join ufr.role
+    where ufr.id.userId = :userId
+""")
+List<AuthMeResponse.FestivalRole> findFestivalRolesForUserId(@Param("userId") Long userId);
+
 }
